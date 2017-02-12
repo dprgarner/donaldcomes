f4e6068a304e228a5fee94f8ac6250e6fee20fb9
David Garner <dprgarner@gmail.com>
2017-02-12 20:12:14 +0000

    Tweet every hour until the world ends

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
 
 const _ = require('lodash');
+const Twit = require('twit');
 
+const {twitterAuth} = require('./auth');
 const zalgo = require('./zalgo');
 
 const SOURCE_WORDS = 'words.txt';
 const SHUFFLED_WORDS = 'words_shuffled.txt';
+
+const client = new Twit(twitterAuth);
 
 function shiftShuffledWord() {
   let wordsString = fs.readFileSync(SHUFFLED_WORDS, 'utf8');
@@ -26,4 +30,37 @@ function constructTweetText(word) {
   }
 }
 
-console.log(constructTweetText(shiftShuffledWord()))
+function updateStatus(params) {
+  return new Promise((resolve, reject) => {
+    client.post('statuses/update', params, function (err, data, response) {
+      if (err) return reject(err);
+      console.log('Updated status');
+      resolve(data);
+    });
+  });
+};
+
+function waitUntilDueTime() {
+  return new Promise((resolve, reject) => {
+    // Wait until the next hour.
+    let tweetInterval = 100;// 1000 * 60 * 60;
+    let msUntilTime = tweetInterval - (Date.now() % tweetInterval);
+    setTimeout(resolve, msUntilTime);
+  });
+}
+
+function waitAndTweet() {
+  let status;
+  while (!status) {
+    status = constructTweetText(shiftShuffledWord()); 
+  }
+  return waitUntilDueTime()
+  .then(() => updateStatus({status}))
+  .then(() => waitAndTweet())
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
+}
+
+waitAndTweet();
